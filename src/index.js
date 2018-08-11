89a4c400045eb695caadd4d6f77430028b036b21
Kirk Tolliver <kirktolliver28@gmail.com>
2018-08-11 18:41:27 -0500

    Initial commit for react project.

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Card from './Card';
+import Cardlist from './Cardlist';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
+import {robots} from './robots.js'
+
+ReactDOM.render(
+                <Cardlist />
+
+  , document.getElementById('root'));
+registerServiceWorker();
