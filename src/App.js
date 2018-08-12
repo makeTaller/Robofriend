0f489c363111b9008f1c5a37f5a6ac708160c3a0
Kirk Tolliver <kirktolliver28@gmail.com>
2018-08-12 10:53:57 -0500

    Provides linking to objects withing scope.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import React, {Component} from 'react';
 import Cardlist from './Cardlist';
-import SearchBox from '.Searchbox';
+import SearchBox from './SearchBox';
 import {robots} from './robots';
 
-class App extends React.Component= () => {
+class App extends Component {
+    constructor() {
+      super()
+      this.state = {
+      robots: robots,
+      searchfield: ''
+    }
+  }
+
+  onSearchChange = (event) => {
+    // this.setState line is neccesary when we make our own functions inside of a class
+    this.setState({searchfield:event.target.value})
+    console.log(event.target.value);
+    console.log(filteredRobots);
+  }
+  render(){
+    const filteredRobots = this.state.robots.filter(robots =>{
+      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+    })
   return(
     <div className='tc'>
       <h1>RoboFriends</h1>
-      <SearchBox />
-      <Cardlist robots={robots}/>
+      <SearchBox searchchange={this.onSearchChange}/>
+      <Cardlist robots={filteredRobots}/>
     </div>
-
-  );
+   );
+  }
 }
 
 export default App;
--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
 const Card = ({ name, email, id }) => {
+	// Must return a value with one tag (div)
 	return (
 	   <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 '>
 				<img alt="robots" src={`https://robohash.org/${id}?200x100`}/>
 						<div>
 							<h2>{name}</h2>
 							<p>{email}</p>
-							</div>
 						</div>
+	  	</div>
 	);
 }
 
