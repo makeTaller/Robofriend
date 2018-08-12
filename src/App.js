5612e5d6dfbf4156078e82504ac31c3e09915507
Kirk Tolliver <kirktolliver28@gmail.com>
2018-08-11 19:55:25 -0500

    Added a robot loop.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
 import React from 'react';
+import Cardlist from './Cardlist';
+import SearchBox from '.Searchbox';
+import {robots} from './robots';
+
+const App = () => {
+  return(
+    <div>
+      <h1>RoboFriends</h1>
+      <Cardlist robots={robots}/>
+    </div>
+
+  );
+}
+
+export default App;
